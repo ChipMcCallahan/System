6d54e3888b4b69bc70fd3586dde480333684c4ed
Chip McCallahan <87612918+ChipMcCallahan@users.noreply.github.com>
2023-04-06 12:07:48 +0000

    boilerplate

---
+++ setup.py
@@ -0,0 +1,22 @@
+"""Setup config for System."""
+from setuptools import setup
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='system',
+    url='https://github.com/ChipMcCallahan/System',
+    author='Chip McCallahan',
+    author_email='thisischipmccallahan@gmail.com',
+    # Needed to actually package something
+    packages=['system'],
+    package_dir={'system': 'src'},
+    # Needed for dependencies
+    install_requires=[],
+    # *strongly* suggested for sharing
+    version='0.1',
+    # The license can be anything you like
+    license='LICENSE',
+    description='Utils intended for author\'s personal use.',
+    # We will also need a readme eventually (there will be a warning)
+    # long_description=open('README.txt').read(),
+)
