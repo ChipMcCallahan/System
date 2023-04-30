8549d1f853ae4c1c859895f6361b8592054e9327
Chip McCallahan <87612918+ChipMcCallahan@users.noreply.github.com>
2023-04-30 04:22:09 +0000

    create and populate tables [harbormaster]

--- src/harbormaster.py
+++ src/harbormaster.py
@@ -1,5 +1,5 @@
 """HarborMaster class."""
-# pylint: disable=invalid-name
+# pylint: disable=invalid-name,line-too-long
 from .sheets_helper import SheetsHelper
 
 class HarborMaster:
@@ -44,4 +44,28 @@ class HarborMaster:
 
     def create_and_populate_global_table(self, name):
         """Populate in harbor db worksheets shared by multiple ships"""
-        return name
+        ships = [ship for ship, wsheets in self.registered_ships().items() if name in wsheets]
+        if len(ships) == 0:
+            print(f"No ships in harbor with wsheet named {name}.")
+            return
+
+        def flatten(l):
+            """https://stackoverflow.com/questions/952914/how-do-i-make-a-flat-list-out-of-a-list-of-lists"""
+            return [item for sublist in l for item in sublist]
+
+        rows = flatten([[row | {"ship": ship} for row in self.read(ship, name)] for ship in ships])
+        if len(rows) == 0:
+            print(f"No items found in all ships under wsheet named {name}.")
+            return
+
+        table = f"[{name}]"
+        self.db.run(f"DROP TABLE IF EXISTS {table}")
+
+        keys = list(rows[0].keys())
+        fields = [f"{key} TEXT" for key in keys]
+        self.db.run(f"CREATE TABLE {table} ({','.join(fields)})")
+
+        values = []
+        for row in rows:
+            values.append(str(tuple(row[key] for key in keys)))
+        self.db.run(f"INSERT INTO {table} VALUES {','.join(values)}")
